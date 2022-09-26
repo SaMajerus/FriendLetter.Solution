e552a5feb543c6ff84940c5555a84de23044e644
Samuel Majerus <ladolego@gmail.com>
2022-09-26 10:33:00 -0700

    Adds a Controller (see 'Controllers' directory).  [End of code-along work -- Lsn 11.]

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    public string Hello() { return "Hello friend!"; }
+    
+    public string Goodbye() { return "Goodbye friend."; }
+
+  }
+}
