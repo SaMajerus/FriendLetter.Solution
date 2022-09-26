9c59a27639464d3a41d93fa96ef834ba1886d037
Samuel Majerus <ladolego@gmail.com>
2022-09-26 10:37:19 -0700

    Adds custom root paths for 'HomeController.cs'.  [End of code-along work -- Lsn 12]

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -5,9 +5,14 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   {
 
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
-    
+
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    public string Letter() { return "Our virtual postcard will go here soon!"; }
 
   }
 }
